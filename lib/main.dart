3076c7ced3af6fda944d66b25e4d744182d6abc8
Rashid Wassan <rashidwassaan@gmail.com>
2022-08-21 01:36:27 +0500

    add hive initialization logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+
 import 'package:attendance_app/attendance_app.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final Directory directory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
   runApp(const MUETAttendanceApp());
 }
